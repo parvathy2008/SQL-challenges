3787510b223beb320bce786058c8f59589219f1c
parvathy2008 <122765443+parvathy2008@users.noreply.github.com>
2023-07-05 12:48:59 +0800

    Create average2.sql

---
+++ Aggregations/average2.sql
@@ -0,0 +1,1 @@
+select b.product_id, round(sum(a.price * b.units)/sum(b.units),2) as average_price from prices a left join  unitssold b on a.product_id =b.product_id where b.purchase_date between a.start_date and  a.end_date group by b.product_id
